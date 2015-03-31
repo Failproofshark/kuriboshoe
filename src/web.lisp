61d2bfefbcb3e9067cf9e617c112b1432b747b89
Bryan Baraoidan <bryan.baraoidan@gmail.com>
2015-03-31 04:53:37 -0700

    Lots of stuff added

--- src/web.lisp
+++ src/web.lisp
@@ -22,8 +22,12 @@
 
 ;;
 ;; Routing rules
+
+;; sanitized-input list -> list
+;; Construct a plist from an alist containing post or get parameters given from a request
 (defun sanitize-input (parameters)
-  (flet ((sanitize-parameter (parameter)
+  (print parameters)
+  (flet ((sanitize-string (parameter)
            (concatenate 'string (loop for char across parameter
                                    if (or (char= #\; char)
                                           (char= #\' char)
@@ -34,41 +38,99 @@
     ;;Note: Is there a bug in sxql? When false is parsed by whatever is handing defroute the parameters
     ;;A symbol is returned for a boolean value which results in an error in execution. The sql statement
     ;;constructed by sxql looks correct so maybe its something with datafly's execute? Either way we
-    ;;convert false to 0 and true to 1;C style which is accepted by mariadb... Look into this
+    ;;convert false to 0 and true to 1; C style which is accepted by mariadb... Look into this
     (loop for parameter in parameters 
-       collect (intern (string-upcase (car parameter)) :keyword)
+       collect (intern (string-upcase (sanitize-string (car parameter))) :keyword)
        collect (let ((parameter-value (cdr parameter)))
-                 (cond ((stringp parameter-value) (sanitize-parameter parameter-value))
+                 (cond ((stringp parameter-value) (sanitize-string parameter-value))
                        ((symbolp parameter-value) (if (eq :false parameter-value)
                                                       0
                                                       1))
                        (t parameter-value))))))
+
+(defun create-insert-statement (table-name user-input)
+  (insert-into table-name
+               (apply #'set= user-input)))
 
 (defun add-new-record (table-name user-input search-column)
   (let* ((sanitized-user-input (sanitize-input user-input))
          (new-id 'nil))
     (if sanitized-user-input
         (progn (with-connection (db)
-                 (execute
-                  (insert-into table-name
-                               (apply #'set= sanitized-user-input)))
-                 (render-json `(:|status| "success" :|newid| ,new-id))
+                 (execute (create-insert-statement table-name sanitized-user-input))
                  (setf new-id (getf (retrieve-one
                                      (select :id
                                              (from table-name)
                                              (where (:= search-column (getf sanitized-user-input search-column)))))
                                     :id)))
-               (render-json `(:|stats| "success" :|newid| ,new-id)))
+               (render-json `(:|status| "success" :|newid| ,new-id)))
         (render-json '(:|status| "error" :|code| "ENONAME")))))
 
-(defroute ("/" :method :get) ()
-  (render #p"index.html"))
+;;Have an optional argument to sort-by
+(defun retrieve-all-from-table (table-name)
+  (with-connection (db)
+    (retrieve-all
+     (select :*
+       (from table-name)))))
 
+;; This is a small work around to what seems to be a bug in datafly. This should only be used for result sets!
+(defun encode-json-custom (result-set)
+  (if (> (length result-set) 1)
+      (encode-json result-set)
+      (concatenate 'string "[" (encode-json (car result-set)) "]")))
+
+;;GET
+(defroute ("/" :method :get) ()
+  (let* ((initial-company-listing (encode-json-custom (retrieve-all-from-table :companies)))
+         (initial-genre-listing (encode-json-custom (retrieve-all-from-table :genres)))
+         (initial-systems-listing (encode-json-custom (retrieve-all-from-table :systems)))
+         (environment-variables (list :companies initial-company-listing
+                                      :genres initial-genre-listing
+                                      :systems initial-systems-listing)))
+    (render #p"index.html" environment-variables)))
+
+;;POST
 (defroute ("/company/" :method :post) (&key _parsed)
   (add-new-record :companies
                   _parsed
                   :name))
-                  
+
+(defroute ("/genre/" :method :post) (&key _parsed)
+  (add-new-record :genres
+                  _parsed
+                  :name))
+
+(defroute ("/system/" :method :post) (&key _parsed)
+  (add-new-record :systems
+                  _parsed
+                  :name))
+
+(defroute ("/games/" :method :post) (&key |genres| |companies| _parsed)
+  (let ((gameparameters (sanitize-input (remove nil (map 'list
+                                                         #'(lambda (parameter)
+                                                             (unless (or (string= (car parameter) "genres")
+                                                                         (string= (car parameter) "companies"))
+                                                               parameter))
+                                                         _parsed))))
+        (new-game-id 'nil))
+    (if (and |genres|
+             |companies|
+             _parsed)
+        (with-connection (db)
+          (execute
+           (create-insert-statement :games gameparameters))
+          (setf new-game-id (getf (retrieve-one
+                              (select :id
+                                      (from :games)
+                                      (where (:= :name (getf gameparameters :name)))))
+                                  :id))
+          (loop for company-id in |companies| do
+               (execute (create-insert-statement :games_companies_pivot `(:game_id ,new-game-id  :company_id ,company-id))))
+          (loop for genre-id in |genres| do
+               (execute (create-insert-statement :games_genres_pivot `(:game_id ,new-game-id :genre_id ,genre-id))))
+          (render-json `(:|status| "success"  :|newid| ,new-game-id)))
+        (render-json '(:|status| "error" :|code| "ENOTFILLED")))))
+
 ;;
 ;; Error pages
 
--- static/js/gametrackeradmin.js
+++ static/js/gametrackeradmin.js
@@ -1,38 +1,147 @@
 var GameTrackerAdmin = {};
 
+//Move things like creating an option and what not into the models. Like "value name" pairs and filter, adding item and autosorting etc.
 var Company = function(initialValues) {
-    var name = m.prop(initialValues.name);
     var id = m.prop(initialValues.id);
+    var name = m.prop(initialValues.name);
 };
 
 var Systems = function(initialValues) {
-    var name = m.prop(initialValues.name);
     var id = m.prop(initialValues.id);
+    var name = m.prop(initialValues.name);
     var manufacturerId = m.prop(initialValues.manufacturerId);
-    var regionId = m.prop(initialValues.regionId);
 };
 
 GameTrackerAdmin.vm = new function() {
     var vm = {};
     vm.init = function() {
-        vm.currentScreen = "add-company";
-
+        vm.formMode = "add";
+        
+        //This is used as a stack;
+        vm.screenHistory = ["GameFormScreen"];
+        
+        //This data is actually bootstraped and the variable it's copying from is in the template
+        vm.companies = companies;
+        vm.genres = genres;
+        vm.systems = systems;
+        
         vm.shouldDisplayScreen = function(screenName) {
-            var displayProperty = (screenName === vm.currentScreen) ? "inherit" : "none";
+            var displayProperty = "none";
+            if (!_.isEmpty(vm.screenHistory)) {
+                displayProperty = (screenName === vm.screenHistory[0]) ? "inherit" : "none";
+            }
             return displayProperty;
+        };
+        vm.createBackButton = function(callback) {
+            return function() {
+                callback();
+                vm.screenHistory.shift();
+            };
         };
 
         vm.newCompanyName = m.prop("");
         vm.isConsoleManufacturer = m.prop(false);
         vm.createNewCompany = function() {
             if (!_.isEmpty(vm.newCompanyName())) {
-                m.request({method: "POST", url: "/company/", data: {name: vm.newCompanyName(), is_manufacturer: vm.isConsoleManufacturer()}})
+                m.request({method: "POST",
+                           url: "/company/",
+                           data: {name: vm.newCompanyName(),
+                                  is_manufacturer: vm.isConsoleManufacturer()}})
                     .then(function(response) {
-                        console.log(response);
                         vm.newCompanyName("");
                         vm.isConsoleManufacturer(false);
                     });
             };
+            return false;
+        };
+        vm.cancelCompanyCreation = vm.createBackButton(function() {
+            vm.newCompanyName("");
+            vm.isConsoleManufacturer(false);
+        });
+
+        vm.newGenreName = m.prop("");
+        vm.createNewGenre = function() {
+            if (!_.isEmpty(vm.newGenreName())) {
+                m.request({method: "POST",  url: "/genre/", data: {name: vm.newGenreName()}})
+                    .then(function(response) {
+                        vm.newGenreName("");
+                    });
+            }
+            return false;
+        };
+        vm.cancelGenreCreation = vm.createBackButton(function() {
+            vm.newGenreName("");
+        });
+
+        vm.newSystemName = m.prop("");
+        vm.newSystemManufacturer = m.prop(null);
+        vm.createNewSystem = function() {
+            if (!_.isEmpty(vm.newSystemName()) && !_.isEmpty(vm.newSystemManufacturer())) {
+                m.request({method: "POST",
+                           url: "/system/",
+                           data:{name: vm.newSystemName(),
+                                 manufacturer_id: vm.newSystemManufacturer()}})
+                .then(function(response) {
+                    console.log(response);
+                    vm.newSystemName("");
+                    vm.newSystemManufacturer(null);
+                });
+            };
+            return false;
+        };
+        vm.cancelSystemCreation = vm.createBackButton(function() {
+            vm.newSystemName = m.prop("");
+            vm.newSystemManufacturer = m.prop(null);
+        });
+
+        vm.selectedEntity = m.prop("");
+
+        //The naming convention seems to have changed (not camel case) but this is because we wish
+        //To mirror what we have in the table, mainly for back-end convenience
+        //TODO have each form have a namespace for their thingies
+        vm.gameForm = {};
+        vm.gameForm.name = m.prop("");
+        vm.gameForm.blurb = m.prop("");
+        vm.gameForm.region = m.prop("");
+        vm.gameForm.has_manual = m.prop(false);
+        vm.gameForm.has_box = m.prop(false);
+        vm.gameForm.notes = m.prop("");
+        vm.gameForm.quantity = m.prop("");
+        vm.gameForm.genres = m.prop([]);
+        vm.gameForm.companies = m.prop([]);
+        vm.gameForm.system_id = m.prop("");
+        
+        vm.createNewGame = function() {
+            console.log("running");
+            if (!_.isEmpty(vm.gameForm.name()) &&
+                !_.isEmpty(vm.gameForm.region()) &&
+                _.isNumber(Number(vm.gameForm.quantity()))
+                && Number(vm.gameForm.quantity()) > 0) {
+                console.log('requestan');
+                m.request({method: "POST",
+                           url: "/games/",
+                           data: vm.gameForm})
+                    .then(function(response) {
+                        console.log("readin response");
+                        console.log(response);
+                        //Reset the form
+                        //TODO probably adapt this to be more universal
+                        _.forEach(vm.gameForm, function(input) {
+                            if (_.isString(input())) {
+                                input("");
+                            } else if (_.isArray(input())) {
+                                input([]);
+                            } else {
+                                input(false);
+                            }
+                        });
+                    });
+                                  
+            }
+            return false;
+        };
+        vm.searchForGame = function() {
+            
             return false;
         };
     };
@@ -43,38 +152,175 @@ GameTrackerAdmin.controller = function() {
     GameTrackerAdmin.vm.init();
 };
 
-GameTrackerAdmin.SimpleTextInputGenerator = function(screenName, inputPlaceHolderText, changeHandler, submitHandler) {
-    return m("form", {display:GameTrackerAdmin.vm.shouldDisplayScreen(screenName)}, [
-        
-        m("input.form-control[type=text]", {placeholder:inputPlaceHolderText, onchange: m.withAttr("value", changeHandler), value: changeHandler()}),
-        m("div", [
-            m("button.btn.btn-success", {onclick: submitHandler}, "submit"),
-            m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.returnToMainForm}, "cancel")
-        ])
-    ]);
+//For use with all Views. Code is based on the one found on mithril's site https://lhorie.github.io/mithril/integration.html
+var select2= {};
+
+/* This factory function offers a nice closure for anything extra we want to pass in */
+select2.config = function(extraArguments) {
+    return function(element, isInitialized, controller) {
+        var el = $(element);
+        if (!isInitialized) {
+            if (extraArguments.select2InitializationOptions) {
+                el.select2(extraArguments.select2InitializationOptions);
+            } else {
+                el.select2();
+            }
+            el.change(function() {
+                m.startComputation();
+                extraArguments.onchange(el.select2("val"));
+                m.endComputation();
+            });
+        }
+        el.select2("val", extraArguments.value);
+    };
 };
 
-GameTrackerAdmin.AddCompanyView = function() {
-    return m("form", {display:GameTrackerAdmin.vm.shouldDisplayScreen("add-company")}, [
-        m("input.form-control[type=text]", {placeholder:"Name", onchange: m.withAttr("value", GameTrackerAdmin.vm.newCompanyName), value: GameTrackerAdmin.vm.newCompanyName()}),
+select2.view = function(extraArguments, optionSet, isMultiple) {
+    var selector = (isMultiple) ? "select.form-control[multiple=true]" : "select.form-control";
+    var createOptionSet = function() {
+        var options = [];
+        if (optionSet) {
+            options = _.map(optionSet, function(value) {
+                var returnValue = (_.isObject(value)) ? m("option", {value: value.id}, value.name) : m("option", value);
+                return returnValue;
+            });
+        }
+        return options;
+    };
+    return m(selector, {config:select2.config(extraArguments)},
+             [m("option"),createOptionSet()]);
+};
+
+GameTrackerAdmin.screenCollection = {};
+
+GameTrackerAdmin.screenCollection.AddCompanyScreen = function() {
+    return m("form", [m("input.form-control[type=text]", {placeholder:"Company Name", onchange: m.withAttr("value", GameTrackerAdmin.vm.newCompanyName), value: GameTrackerAdmin.vm.newCompanyName()}),
+            m("div.checkbox", [
+                m("label", [
+                    m("input[type=checkbox]", {onchange: m.withAttr("checked", GameTrackerAdmin.vm.isConsoleManufacturer), checked: GameTrackerAdmin.vm.isConsoleManufacturer()})
+                ]),
+                m("span", "Is this company a console manufacuturer?")
+            ]),
+            m("div", [
+                m("button.btn.btn-success", {onclick: GameTrackerAdmin.vm.createNewCompany}, "submit"),
+                m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.returnToMainForm}, "cancel")
+            ])]);
+};
+
+GameTrackerAdmin.screenCollection.AddGenreScreen = function() {
+    return m("form", [ m("input.form-control[type=text]", {placeholder:"Genre Name", onchange: m.withAttr("value", GameTrackerAdmin.vm.newGenreName), value: GameTrackerAdmin.vm.newGenreName()}),
+             m("div", [
+                 m("button.btn.btn-success", {onclick: GameTrackerAdmin.vm.createNewGenre}, "submit"),
+                 m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.returnToMainForm}, "cancel")
+             ])
+           ]);
+};
+
+GameTrackerAdmin.screenCollection.AddSystemScreen = function() {
+    var createManufacturerSet = function() {
+        return _.map(_.filter(GameTrackerAdmin.vm.companies, {'isManufacturer':1}), function(company) {
+            return {value: company.id, text: company.name};
+        });
+    };
+
+    return m("form", [m("input.form-control[type=text]", {placeholder:"System Name", onchange: m.withAttr("value", GameTrackerAdmin.vm.newSystemName), value: GameTrackerAdmin.vm.newSystemName()}),
+                      m("div", [
+                          select2.view({ onchange:GameTrackerAdmin.vm.newSystemManufacturer,
+                                         value:GameTrackerAdmin.vm.newSystemManufacturer(),
+                                         select2InitializationOptions:{placeholder:"Manufacturer"}},
+                                       createManufacturerSet()),
+                          m("u[style=cursor:pointer]", {onclick: GameTrackerAdmin.vm.screenHistory.unshift.bind(GameTrackerAdmin.vm.screenHistory, "AddCompanyScreen")}, "+Add Company")
+                      ]),
+                      m("div", [
+                          m("button.btn.btn-success", {onclick: GameTrackerAdmin.vm.createNewSystem}, "submit"),
+                          m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.returnToMainForm}, "cancel")
+                      ])]);
+};
+
+GameTrackerAdmin.screenCollection.GameFormScreen = function() {
+    var formConfiguration = {
+        textAreaDisplay: function() {
+            var displayValue = (GameTrackerAdmin.vm.formMode === "search") ? "display:none" : "display:inherit";
+            return displayValue;
+        },
+        confirmButtonHandler: function() {
+            console.log('called');
+            var handler;
+            switch (GameTrackerAdmin.vm.formMode) {
+            case "add":
+                console.log('yay');
+                handler = GameTrackerAdmin.vm.createNewGame;
+                break;
+            case "search":
+                console.log('wat');
+                handler = GameTrackerAdmin.vm.searchForGame;
+                break;
+            }
+            return handler;
+        }
+    }
+    return m("form", [
+        m("input.form-control", {onchange: m.withAttr("value", GameTrackerAdmin.vm.gameForm.name),
+                                 value: GameTrackerAdmin.vm.gameForm.name(),
+                                 placeholder: "Name"}),
+        select2.view({onchange:GameTrackerAdmin.vm.gameForm.region,
+                      value: GameTrackerAdmin.vm.gameForm.region(),
+                      select2InitializationOptions: {placeholder: "Region"}},
+                     ["NTSC", "NTSC-J", "PAL"]),
+        select2.view({onchange:GameTrackerAdmin.vm.gameForm.system_id,
+                      value: GameTrackerAdmin.vm.gameForm.system_id(),
+                      select2InitializationOptions: {placeholder: "System"}},
+                     GameTrackerAdmin.vm.systems),
+        select2.view({onchange:GameTrackerAdmin.vm.gameForm.genres,
+                      value: GameTrackerAdmin.vm.gameForm.genres(),
+                      select2InitializationOptions: {placeholder: "Genres"}},
+                     GameTrackerAdmin.vm.genres,
+                     true),
+        select2.view({onchange:GameTrackerAdmin.vm.gameForm.companies,
+                      value: GameTrackerAdmin.vm.gameForm.companies(),
+                      select2InitializationOptions: {placeholder: "Companies"}},
+                     GameTrackerAdmin.vm.companies,
+                     true),
+        m("input.form-control", {onchange: m.withAttr("value", GameTrackerAdmin.vm.gameForm.quantity),
+                                 value: GameTrackerAdmin.vm.gameForm.quantity(),
+                                 placeholder: "Quantity"
+                                }),
+        
+        m("div", {style:formConfiguration.textAreaDisplay()}, [
+            m("p", "Short Description"),
+            m("textarea", {onchange: m.withAttr("value", GameTrackerAdmin.vm.gameForm.blurb)}, GameTrackerAdmin.vm.gameForm.blurb()),
+        ]),
         m("div.checkbox", [
             m("label", [
-                m("input[type=checkbox]", {onchange: m.withAttr("checked", GameTrackerAdmin.vm.isConsoleManufacturer), checked: GameTrackerAdmin.vm.isConsoleManufacturer()})
+                m("input[type=checkbox]", {onchange: m.withAttr("checked", GameTrackerAdmin.vm.gameForm.has_manual), checked: GameTrackerAdmin.vm.gameForm.has_manual()})
             ]),
-            m("span", "Is this company a console manufacuturer?")
+            m("span", "Manual")
         ]),
+        m("div.checkbox", [
+            m("label", [
+                m("input[type=checkbox]", {onchange: m.withAttr("checked", GameTrackerAdmin.vm.gameForm.has_box), checked: GameTrackerAdmin.vm.gameForm.has_box()})
+            ]),
+            m("span", "Box")
+        ]),
+        m("div", {style:formConfiguration.textAreaDisplay()}, [
+            m("p", "Notes"),
+            m("textarea", {onchange: m.withAttr("value", GameTrackerAdmin.vm.gameForm.notes)}, GameTrackerAdmin.vm.gameForm.notes()),
+        ]),        
         m("div", [
-            m("button.btn.btn-success", {onclick: GameTrackerAdmin.vm.createNewCompany}, "submit"),
-            m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.returnToMainForm}, "cancel")
+            m("button.btn.btn-success", {onclick: formConfiguration.confirmButtonHandler()}, "submit"),
+            m("button.btn.btn-danger", {onclick: GameTrackerAdmin.vm.cancelNewGameCreation}, "cancel")
         ])
     ]);
 };
 
 GameTrackerAdmin.view = function() {
+    var renderScreens = function() {
+        return _.map(GameTrackerAdmin.screenCollection, function(screenContent, screenName) {
+            return m("div", {style:"display:"+GameTrackerAdmin.vm.shouldDisplayScreen(screenName)}, screenContent());
+        });
+    };
     return m("div.row", [
-        m("div.col-xs-12", [
-            GameTrackerAdmin.AddCompanyView()
-        ])
+        m("div.col-xs-12", renderScreens())
     ]);
 };
 
